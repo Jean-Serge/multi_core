756df4e2ebc2b23008007513e96371d4dcaf5b34
arctarus44 <arthur.dewarumez@gmail.com>
2015-02-16 12:08:24 +0100

    Fin de l'exercice 2.

--- include/hw_info.h
+++ include/hw_info.h
@@ -6,5 +6,9 @@
 #define CORE_ID         0x126
 #define CORE_LOCK       0x98
 #define CORE_UNLOCK     0x99
+#define TIMER_IRQ       2
+#define CORE_IRQMAPPER  0x82
+#define TIMER_ALARM     0xF8
+#define TIMER_PARAM     0xF4
 
 #endif
---
+++ src/active-core-int.c
@@ -0,0 +1,53 @@
+#include "hardware.h"
+#include "hw_info.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void function(){
+	int i;
+	_mask(1);
+	while(1){
+		printf("core %d\n", _in(CORE_ID));
+		for(i = 0; i < 1<<25; i++){}
+	}
+}
+
+void print_IRQ(){
+	printf("## Intéruption %d ##\n", _in(CORE_ID));fflush(stdout);
+	_out(TIMER_ALARM, 0xFFFFFFFF - 100);
+}
+
+void empty_irq(){
+	return;
+}
+
+int main(int argc, char *argv[]){
+	int i;
+	if(init_hardware("etc/core.ini") == 0){
+		printf("Erreur dans l'initialisation du hardware.\n");
+		return 1;
+	}
+
+	for(i = 1; i < 16; i++)
+		IRQVECTOR[i] = empty_irq;
+
+	IRQVECTOR[0] = function;
+
+	/* On active les 4 premiers core 0xF -> 1111 */
+	_out(CORE_STATUS, 0x3F);
+
+	IRQVECTOR[TIMER_IRQ] = print_IRQ;
+	for(i = 0; i < 6; i++)
+		_out(CORE_IRQMAPPER+i, 0);
+
+	_out(CORE_IRQMAPPER+2, 1<< TIMER_IRQ);/* On redirige l'IRQ TIMER vers le core 2 */
+
+	_out(TIMER_PARAM, 128 + 64 /* + 32 +8 */);
+	_out(TIMER_ALARM, 0xFFFFFFFF - 20);
+
+	/* _mask(1); */
+	function();
+
+	return 0;
+}
